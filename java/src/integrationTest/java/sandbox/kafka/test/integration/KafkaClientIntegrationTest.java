bab98355737ee39d8691522d6478de685cdb1538
Corey Fritz <corey.fritz@gmail.com>
2020-05-03 22:59:50 -0400

    Added a Kafka producer/consumer integration test using Testcontainers

---
+++ java/src/integrationTest/java/sandbox/kafka/test/integration/KafkaClientIntegrationTest.java
@@ -0,0 +1,99 @@
+package sandbox.kafka.test.integration;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.Test;
+import sandbox.kafka.consumer.Consumer;
+import sandbox.kafka.consumer.ConsumerConfig;
+import sandbox.kafka.producer.Producer;
+import sandbox.kafka.producer.ProducerConfig;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * This integration test verifies that we can send and receive a message through Kafka.
+ *
+ */
+public class KafkaClientIntegrationTest extends KafkaIntegrationTest {
+
+    private static final String TOPIC = "test-topic";
+
+    @Test
+    public void testProducerAndConsumer() {
+        // create Kafka producer
+        Producer<String, String> producer = createProducer();
+
+        // test message
+		String key = "key";
+		String value = "test";
+
+        // send message
+		AtomicReference<RecordMetadata> metadata = new AtomicReference<>();
+		AtomicReference<Exception> exception = new AtomicReference<>();
+        producer.send(key, value, (meta, ex) -> {
+        	metadata.set(meta);
+        	exception.set(ex);
+		});
+
+        // close Kafka producer
+        producer.close();
+
+		// verify successful receipt
+		assertNull(exception.get());
+		assertNotNull(metadata.get());
+		assertEquals(TOPIC, metadata.get().topic());
+		assertEquals(0, metadata.get().partition());
+		assertEquals(0, metadata.get().offset());
+		assertTrue(metadata.get().hasTimestamp());
+
+        // create Kafka consumer
+        Consumer<String, String> consumer = createConsumer();
+
+        // poll once for messages
+        ConsumerRecords<String, String> poll = consumer.poll();
+
+        // close Kafka consumer
+        consumer.close();
+
+        // verify results
+        assertNotNull(poll);
+        assertEquals(1, poll.count());
+
+		ConsumerRecord<String, String> record = poll.iterator().next();
+        assertNotNull(record);
+        assertEquals(TOPIC, record.topic());
+        assertEquals(0, record.partition());
+        assertEquals(0, record.offset());
+        assertTrue(record.timestamp() > 0);
+        assertEquals(key, record.key());
+        assertEquals(value, record.value());
+    }
+
+    private Producer<String, String> createProducer() {
+        ProducerConfig config = new ProducerConfig();
+        config.setBrokers(kafka.getBootstrapServers());
+        config.setTopic(TOPIC);
+        config.setKeySerializer(StringSerializer.class);
+        config.setValueSerializer(StringSerializer.class);
+        return new Producer<>(config);
+    }
+
+    private Consumer<String, String> createConsumer() {
+        ConsumerConfig config = new ConsumerConfig();
+        config.setBrokers(kafka.getBootstrapServers());
+        config.setGroup("test-group");
+        config.setTopic(TOPIC);
+        config.setKeyDeserializer(StringDeserializer.class);
+        config.setValueDeserializer(StringDeserializer.class);
+        config.addProperty("auto.offset.reset", "earliest");
+        return new Consumer<>(config);
+    }
+}
